c929f505cfd8da72f45ad5668092f8c47eb0772c
Sublime-Sweetie <faith.Wallace.2@gmail.com>
2022-12-06 20:09:35 -0800

    Server.js and db added

---
+++ server.js
@@ -0,0 +1,27 @@
+"What would you like to do?"
+"What is the name of the department"
+"What is the name of this role?"
+"What is the salary of this role?"
+"What is the employee's first name?"
+"What is the employee's last name?"
+"What is this employee's role?"
+"Who is the employee's manager?"
+
+// View All Employees
+// Add Employee
+// Update Employee Role
+// View All Roles
+// Add Role
+// View All Departments
+// Add Department
+
+// Sales Lead
+// Salesperson
+// Lead Engineer
+// Software Engineer
+// Account Manager
+// Accountant
+// Legal Team Lead
+// Lawyer
+
+
